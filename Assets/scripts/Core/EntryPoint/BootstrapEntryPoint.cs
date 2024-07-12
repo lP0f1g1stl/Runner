f2f51e500853baca2d492dd46ca80bd7d3f30173
lP0f1g1stl <64088803+lP0f1g1stl@users.noreply.github.com>
2024-07-12 11:38:15 +0300

    Zenject + Unitask + Address

---
+++ Assets/scripts/Core/EntryPoint/BootstrapEntryPoint.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.EntryPoint
+{
+    public class BootstrapEntryPoint : MonoBehaviour
+    {
+
+    }
+}
